e5b09c9678010f0edda40dbe93119597d158c208
andrewm <andrewmillercode@gmail.com>
2025-02-11 00:03:12 -0800

    fixed placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
       subTitle:"Vue, Chrome, Gemini",
       imgLocation: "/autofill.png",
       url: "https://github.com/andrewmillercode/Autofill-Jobs",
-      description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum",
+      description: "A chrome extension that autofills job applications, built with Vue.",
     },
     { Title:"Snappit",
         subTitle:"AWS S3, Next.js, Supabase, PostgreSQL, Prisma, Gemini, REST APIs",
